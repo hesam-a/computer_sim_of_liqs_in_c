49282ea5a5c07189217811bb9cd94ea4bbe7161d
Hesam Arabzadeh <40280976+hesam-a@users.noreply.github.com>
2022-05-01 00:33:27 -0400

    Update t_tensors.cpp

--- t_tensors.cpp
+++ t_tensors.cpp
@@ -23,10 +23,27 @@
    multipole on the other molecule. */
 #include <iostream>
 
+
+    double alpha = 1.0, beta = 0.0;
+    double *C;
+    C = (double*) malloc(nRows * nCols * sizeof(double));
+    for (i = 0; i < nRows*nCols; i++) C[i] = 0.0;
+
+    cblas_dger(CblasRowMajor, nRows, nCols, alpha, A, 1, B, 1, C, nCols);
+
+
+
    // First the tensor functions will be defined
 int t2_tensor (r,r3){
 /* Returns second-rank 3x3 interaction tensor.
     Supplied arguments should be the unit vector from 2 to 1 and
     the third power of the modulus of that vector.*/
    
+}
+
+int main(int agrc, char* argv[]){
+
+    free(C);
+
+    return 0;
 }
