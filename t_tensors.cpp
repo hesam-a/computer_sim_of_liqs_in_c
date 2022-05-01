c1fe226bae8264c1608ae5a4497aa1568b960860
Hesam Arabzadeh <40280976+hesam-a@users.noreply.github.com>
2022-05-01 00:35:55 -0400

    Update t_tensors.cpp

--- t_tensors.cpp
+++ t_tensors.cpp
@@ -22,18 +22,31 @@
    It is just necessary to identify the constants g_i, G_ij, in terms of the T tensor and the
    multipole on the other molecule. */
 #include <iostream>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <math.h>
+#include <mkl.h>
+
+#define nCols 5
+#define nRows 19
+#define k 1
 
 
-    double alpha = 1.0, beta = 0.0;
-    double *C;
-    C = (double*) malloc(nRows * nCols * sizeof(double));
-    for (i = 0; i < nRows*nCols; i++) C[i] = 0.0;
 
-    cblas_dger(CblasRowMajor, nRows, nCols, alpha, A, 1, B, 1, C, nCols);
+double alpha = 1.0, beta = 0.0;
+double *C;
+C = (double*) malloc(nRows * nCols * sizeof(double));
+for (i = 0; i < nRows*nCols; i++) C[i] = 0.0;
+
+cblas_dger(CblasRowMajor, nRows, nCols, alpha, A, 1, B, 1, C, nCols);
 
 
 
-   // First the tensor functions will be defined
+// The tensor functions are defined here in order
+
 int t2_tensor (r,r3){
 /* Returns second-rank 3x3 interaction tensor.
     Supplied arguments should be the unit vector from 2 to 1 and
