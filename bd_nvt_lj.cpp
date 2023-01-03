534d72b4dba229412208aa3b4587a05a7ce8d44d
Hesam Arabzadeh <40280976+hesam-a@users.noreply.github.com>
2023-01-02 20:30:02 -0500

    Update bd_nvt_lj.cpp

--- bd_nvt_lj.cpp
+++ bd_nvt_lj.cpp
@@ -5,28 +5,13 @@
 #include <vector>
 #include <ctime>
 #include <cassert>
+#include <iomanip>
 #include "./lrc_module.hpp"
 #include "./maths_module.hpp"
 #include "./md_lj_module.hpp"
 #include "./averages_module.hpp"
 #include "./config_io_module.hpp"
 
-/*  Takes in a configuration of atoms (positions, velocities)
-    Cubic periodic boundary conditions
-    Conducts molecular dynamics using BAOAB algorithm of BJ Leimkuhler and C Matthews
-    Appl. Math. Res. eXpress 2013, 34–56 (2013); J. Chem. Phys. 138, 174102 (2013)
-    Uses no special neighbour lists
-
-    Reads several variables and options from standard input using JSON format
-    Leave input empty "{}" to accept supplied defaults
-
-    Positions r are divided by box length after reading in and we assume mass=1 throughout
-    However, input configuration, output configuration, most calculations, and all results 
-    are given in simulation units defined by the model
-    For example, for Lennard-Jones, sigma = 1, epsilon = 1
-
-    Despite the program name, there is nothing here specific to Lennard-Jones
-    The model is defined in md_lj_module*/
 
 #define nblock        10
 #define nstep         1000
@@ -35,7 +20,7 @@
 #define dt            0.005
 #define gamma         1.0
 
-std::vector<VariableType> calc_variables(PotentialType tot, double** r, int n, double box ){
+std::vector<VariableType> calc_variables(PotentialType tot, double** r, int n, double box, double** vel, double** f ){
 /*
      --- some notes from Allen-Tildesley ---
      Variables of interest, of class VariableType, containing three attributes:
@@ -50,12 +35,16 @@ std::vector<VariableType> calc_variables(PotentialType tot, double** r, int n, d
     // Preliminary calculations (n,v,f,total are taken from the calling program)
     double vol = pow(box,3);                        //  Volume
     double rho = n / vol;                           //  Density
-    double kin = 0.5*                               //  Kinetic energy
-    double fsq = pow(force (tot, n, box, r_cut, r),2);   //  Total squared force from md_lj_module
-    // std::cout << " ---- force:   " << fsq << " ---- \n";
+    double** f_sq;
+    double** vel_sq;
+    matMultip(n,3,vel,vel,vel_sq);
+    double vel_sum = elementSum2D(n,3, vel_sq);
+    double kin = 0.5 * vel_sum;                     //  Kinetic energy    
+    force (tot, n, box, r_cut, r, f);              
+    matMultip(n,3,f,f,f_sq);                         //  Total squared force from md_lj_module
+    double fsq = elementSum2D(n,3, f_sq);
 
-    // Initial energy and overlap check
-    potential (tot,n, box, r_cut, r);
+    // std::cout << " ---- force:   " << fsq << " ---- \n";
 
     // Internal energy (cut-and-shifted) per atom
     // Total KE plus total cut-and-shifted PE divided by N
@@ -71,9 +60,9 @@ std::vector<VariableType> calc_variables(PotentialType tot, double** r, int n, d
 
     // Pressure (cut-and-shifted)
     // Ideal gas contribution plus total virial divided by V
-    VariableType p_c;
-    p_c.nam = "P cut&shifted";
-    p_c.val = rho*temperature + tot.vir/vol;
+    VariableType p_s;
+    p_s.nam = "P cut&shifted";
+    p_s.val = rho*temperature + tot.vir/vol;
 
     // Pressure (full, including LRC)
     // LRC plus ideal gas contribution plus total virial divided by V
@@ -85,7 +74,7 @@ std::vector<VariableType> calc_variables(PotentialType tot, double** r, int n, d
     // Momentum is not conserved, hence 3N degrees of freedom
     VariableType t_k;
     t_k.nam = "T kinetic";
-    t_k.val = val = 2.0*kin/(3*n);
+    t_k.val = 2.0*kin/(3*n);
 
     // Configurational temperature
     // Total squared force divided by total Laplacian
@@ -116,12 +105,30 @@ std::vector<VariableType> calc_variables(PotentialType tot, double** r, int n, d
     variables.push_back(e_f);
     variables.push_back(p_s);
     variables.push_back(p_f);
+    variables.push_back(t_k);
     variables.push_back(t_c);
-    variables.push_back(t_f);
     variables.push_back(c_s);
     variables.push_back(c_f);
 
+    delete [] f_sq;
+    delete [] vel_sq;
+
     return variables;
+
+}
+
+void deletePointer(std::vector<VariableType> vars, BlockVar blk_var){
+//  A function for deleting the VariableType objects called by calc_variables
+
+    // delete blockVariable's instances
+    delete [] blk_var.run_avg;
+    delete [] blk_var.run_err;
+    delete [] blk_var.blk_avg;
+    delete [] blk_var.blk_msd;
+    delete [] blk_var.values;
+    delete [] blk_var.addd;
+    delete [] blk_var.mask;
+    delete [] blk_var.methodd;
 
 }
 
@@ -129,22 +136,22 @@ void a_propagator(double time, int n, double box, double** r, double** vel){
 /*    A: drift step propagator.
     t is the time over which to propagate (typically dt/2). */
 
-    scalar2DArrayMultip(n,3,t/box,vel);    // (t/box)*velocity
+    scalar2DArrayMultip(n,3,time/box,vel,vel);    // (t/box)*velocity
     sum2DArrays(n,3,vel,r);                // Positions in box=1 units
     rint2D(n,3,r);                         // Periodic boundaries
 }
 
-void b_propagator(potentialType tot, double time, int n, double box, double** vel, double** r, double** f){
+void b_propagator(PotentialType tot, double time, int n, double box, double** vel, double** r, double** f){
 /*  B: kick step propagator.
     t is the time over which to propagate (typically dt/2).
     v is accessed from the calling program. */
-    f = force (tot, n, box, r_cut, r);
-    scalar2DArrayMultip(n,3,t,f);    // t * force
-    matMultip(n,3,vel,f,vel);    // t * force
+    force (tot, n, box, r_cut, r, f);
+    scalar2DArrayMultip(n,3,time,f,f);    // t * force
+    matMultip(n,3,vel,f,vel);             // t * force
     sum2DArrays(n,3,vel,vel);
 }
 
-void o_propagator ( double t, double** vel, int n ){
+void o_propagator ( double time, double** vel, int n ){
 /* O: friction and random contributions propagator.
 
     t is the time over which to propagate (typically dt).
@@ -152,23 +159,39 @@ void o_propagator ( double t, double** vel, int n ){
 
     double** rnd;
     rand2DArray(n,3,rnd);
-    double x = gamma*t
+    double x = gamma*time;
     double c;
     if (x > 0.0001)
         c = 1-exp(-2*x);
     else 
         c = -2/3*pow(x,4)+4/3*pow(x,3)-2.0*pow(x,2)+2.0*x;
     
-    matMultip(n,3,c,c);
-    scalar2DArrayMultip(n,3,exp(-x),vel);
-    matMultip(n,3,sqrt(temperature),c);
-    scalar2DArrayMultip(n,3,c,rnd);
+    c = sqrt(c);
+    scalar2DArrayMultip(n,3,exp(-x),vel,vel);
+    scalar2DArrayMultip(n,3,c*sqrt(temperature),rnd,rnd);
     sum2DArrays(n,3,rnd,vel);
+    delete [] rnd;
 }
 
 
-
 int main(){
+
+    /*  Takes in a configuration of atoms (positions, velocities)
+    Cubic periodic boundary conditions
+    Conducts molecular dynamics using BAOAB algorithm of BJ Leimkuhler and C Matthews
+    Appl. Math. Res. eXpress 2013, 34–56 (2013); J. Chem. Phys. 138, 174102 (2013)
+    Uses no special neighbour lists
+
+    Reads several variables and options from standard input using JSON format
+    Leave input empty "{}" to accept supplied defaults
+
+    Positions r are divided by box length after reading in and we assume mass=1 throughout
+    However, input configuration, output configuration, most calculations, and all results 
+    are given in simulation units defined by the model
+    For example, for Lennard-Jones, sigma = 1, epsilon = 1
+
+    Despite the program name, there is nothing here specific to Lennard-Jones
+    The model is defined in md_lj_module. */
 
     // initial time for calculating the processing time
     std::clock_t ti = std::clock();
@@ -188,9 +211,10 @@ int main(){
 
     int na   = 3;
     int ndim = 3;
-    double** r      = allocate2DArray(n,3);
+    double** r      = allocate2DArray(n,3); // position
+    double** f      = allocate2DArray(n,3); // force
     double** e      = allocate2DArray(n,4);
-    double** vel    = allocate2DArray(n,3);
+    double** vel    = allocate2DArray(n,3); // velocity
     double** angvel = allocate2DArray(n,3);
     bool quaternion = false;
     bool with_v     = true;
@@ -202,25 +226,28 @@ int main(){
 
     // Initial forces, potential, etc plus overlap check
     PotentialType total;
-
-    potential (total,n, box, r_cut, r);
+    force (total,n, box, r_cut, r, f);
 
     assert (!total.ovr); 
     std::cout << "No overlap in initial configuration! \n";
-        
+
+    BlockVar blk_var;
+    
+    run_begin (calc_variables(total, r, n, box, vel, f), blk_var, ti);
+
     std::cout << '\n';
     std::cout << "bd_nvt_lj \n";
     std::cout << "Brownian dynamics, constant-NVT ensemble \n";
     std::cout << "Particle mass=1 throughout \n";
     std::cout << '\n';
 
-    introduction()
+    introduction();
 
     //  Write out parameters
     std::cout << '\n';
     printf("%16s %42d   \n", "Number of blocks",            nblock);
     printf("%25s %33d   \n", "Number of steps per block",   nstep);
-    printf("%25s %33d   \n", "Potential cutoff distance",   r_cut);
+    printf("%25s %33f   \n", "Potential cutoff distance",   r_cut);
     printf("%20s %36.6f \n", "Time Step",                   dt);
     printf("%20s %36.6f \n", "Friction coeffcient",         gamma);
     printf("%20s %37.6f \n", "Specified temperature",       temperature);
@@ -233,22 +260,55 @@ int main(){
     printf("%7s   %50.6f \n", "Density", n/pow(box,3));
     std::cout << '\n'; 
 
-    BlockVar blk_var;
-    
+    int n_avg = calc_variables(total, r, n, box, vel, f).size();
+
     // Initialize arrays for averaging and write column headings
-    run_begin (calc_variables(total, r, n, box), blk_var, ti);
+    run_begin (calc_variables(total, r, n, box, vel, f), blk_var, ti);
 
     for (int blk{0}; blk < nblock;++blk){                                // Loop over blocks
 
-	  blk_begin(n_avg,blk_var); // from md_lj_module
+	    blk_begin(n_avg,blk_var);
 
         for (int stp{0}; stp<nstep;++stp){                               // Loop over steps    
 
-        b_propagator ( dt/2 ) // B kick half-step
-        a_propagator ( dt/2 ) // A drift half-step
-        o_propagator ( dt )   // O random velocities and friction step
-        a_propagator ( dt/2 ) // A drift half-step
+ 	    b_propagator(total, dt/2, n, box, vel, r, f);// B kick half-step
+            a_propagator ( dt/2, n, box, r, vel );      // A drift half-step
+            o_propagator ( dt, vel,  n );               // O random velocities and friction step
+            a_propagator ( dt/2, n, box, r, vel );      // A drift half-step
 
-          }
+            force (total,n, box, r_cut, r, f);
+
+            assert (!total.ovr); 
+            std::cout << "No overlap in configuration! \n";
+
+            a_propagator ( dt/2, n, box, r, vel ); // A drift half-step
+
+            blk_add (calc_variables(total, r, n, box, vel, f),blk_var);
+
+        }
+	
+        blk_end ( blk, n_avg, blk_var);                         // Output block averages
+        std::stringstream ss;
+        ss << std::setfill('0') << std::setw(3) << std::to_string(blk+1);
+        std::string sav_tag(ss.str());                      // Number configuration by block
+        double** out_r = allocate2DArray(n,3);
+        scalar2DArrayMultip(n,3,box,r,out_r);
+        write_cnf_atoms ("cnf."+sav_tag, n, box,out_r );    // Save configuration
+        free2DArray(n,out_r);
     }
+    run_end (calc_variables(total, r, n, box, vel, f), blk_var, ti);
+    force (total,n, box, r_cut, r, f);
+
+    assert (!total.ovr); 
+    std::cout << "No overlap in final configuration! \n";
+
+    double** out_r = allocate2DArray(n,3);
+    scalar2DArrayMultip(n,3,box,r,out_r);
+    write_cnf_atoms ("cnf.out", n, box,out_r );            // Save configuration
+    free2DArray(n,out_r);
+    deletePointer(calc_variables(total, r, n, box, vel, f), blk_var);
+
+    conclusion();
+    free2DArray(n,r);
+
 }
